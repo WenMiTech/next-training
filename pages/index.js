0f57e6df755a3c3a4e689e7b89fdf22b2127cf13
zenggr <LyonRu@LyonRudeMacBook-Pro.local>
2018-07-12 16:54:29 +0800

    navigate pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import Link from 'next/link';
+
 const Index = () => {
 
-    return (<h1>Welcome next.js</h1>)
+    return (
+        <div>
+            <h1>Welcome next.js</h1>
+            {/* no effect */}
+            <Link href="/about" style={{ fontSize: 20 }}><a>about</a></Link>
+            <Link href="/about"><button>about</button></Link>
+        </div>
+    )
 }
 
 export default Index;
